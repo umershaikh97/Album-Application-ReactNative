ffb6dc39fa60cc21db59f4515ecd6082636c3b67
umershaikh97 <umershaikh305@gmail.com>
2019-05-16 21:57:08 +0500

    cards done

--- components/AlbumDetail.js
+++ components/AlbumDetail.js
@@ -1,31 +1,68 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Card from './Card';
-import CardItem from './CardItem';
-
 
 const AlbumDetail = (props) =>{
     return(
-        <Card>
+        <View style={styles.cardStyle}>
             
-            <CardItem>
-            <Text style={styles.textStyle} > {props.album.title} </Text>
-            </CardItem>
+            <View style={styles.cardSectionStyle}>
+            
+            <View style={styles.picContainerStyle}>
+                <Text>Pic here</Text>
+            </View>
 
+
+            <View style={styles.albumContainerStyle}>
+            <Text>{props.album.title}</Text>
+            <Text>{props.album.artist}</Text>
+            </View>
+
+            </View>
             
-        </Card>
+        </View>
     );
 
 
 }
 
 
+
 const styles = StyleSheet.create({
     textStyle: {
         color: 'black',
         fontSize: 15   
-    }
-})
+    },
+
+    picContainerStyle : {
+        backgroundColor: 'yellow',
+        height: 50,
+        width: 90
+    },
+    
+    albumContainerStyle : {
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        marginLeft: 10
+    },
+
+    cardSectionStyle : {
+        borderBottomWidth: 1,
+        padding: 5,
+        backgroundColor: '#fff',
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        borderColor: '#ddd',
+        position: 'relative'
+    },
+
+    cardStyle : {
+        borderWidth: 1,  // border 1px
+        borderRadius: 2,  // rounded corners
+        borderColor: '#ddd',
+        marginTop: 10,
+        marginLeft: 5,
+        marginRight: 5  }
+});
 
 
 
